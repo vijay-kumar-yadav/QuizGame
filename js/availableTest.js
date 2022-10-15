907b1fde612bef282df505e492cfa8e857a3a1e6
vijay-kumar-yadav <vky405021@gmail.com>
2022-10-15 09:18:06 +0530

    project uploaded

---
+++ js/availableTest.js
@@ -0,0 +1,122 @@
+var testType = {
+    Books: 10,
+    Film: 11,
+    Music: 12,
+    // "Entertainment:_Musicals_&_Theatres": 13,
+    Television: 14,
+    Video_Games: 15,
+    Board_Games: 16,
+    Comics: 29,
+    Nature: 17,
+    Computers: 18,
+    Gadgets: 30,
+    Mathematics: 19,
+    Mythology: 20,
+    General_Knowledge: 9,
+    Sports: 21,
+    Geography: 22,
+    History: 23,
+    Politics: 24,
+    Art: 25,
+    Celebrities: 26,
+    Animals: 27,
+    Vehicles: 28,
+};
+
+async function fetchApi(query) {
+    const clientId = "hE7WN5evihVZ2U2yYOXhMjb-ZHb5NahT8Ml0S-Wwylo";
+    const url =
+        "https://api.unsplash.com/search/photos?page=1&query=" +
+        query +
+        "&per_page=2&client_id=" +
+        clientId;
+    const imageDiv = document.querySelector(".image");
+    const imageUrl = await fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let imageElement = data.results[0].urls.small;
+
+            return imageElement;
+        });
+    return imageUrl;
+}
+async function testCardSkeleton(id, Name) {
+    var testCardContainer = $("<div></div>")
+        .addClass("testCard col-md-3 col-sm-6 ")
+        .attr("id", "testCardContainer" + id);
+    var testCards = $("<div></div>").addClass("testCards card ");
+    var testCardsBody = $("<div></div>").addClass("card-body");
+    var testCardsImg = $("<div></div>").addClass("testCardsImg");
+    var img_avatar = $("<img></img>")
+        .attr("src", await fetchApi(Name))
+        .addClass("w-100  border-bottom")
+        .css("height", "200px");
+    var testName = $("<div></div>").addClass("testName");
+    var courseCode = $("<p></p>")
+        .attr("id", "courseCode")
+        .text(Name)
+        .addClass("text-center");
+    var startTestBtn = $("<button></button>")
+        .attr("id", "startTest" + id)
+        .addClass("startTest")
+        .text("Start Quiz");
+    startTestBtn.click(() => startCountDown(id, Name));
+    testCardsImg.append(img_avatar);
+    testName.append(courseCode);
+    testCardsBody.append(testCardsImg, testName);
+    testCards.append(testCardsBody, startTestBtn);
+    testCardContainer.append(testCards);
+    $(".allTestContainer").append(testCardContainer);
+}
+for (let key of Object.keys(testType)) {
+    testCardSkeleton(
+        testType[key],
+        key.replace("_", " ").replace("_", " ")
+    );
+}
+
+var count = 4;
+$("#quesContainer").hide();
+var intr = 0;
+function countNum() {
+    let e = document.querySelector(".counter");
+    let time = 5;
+    intr = setInterval(() => {
+        if (time == 0) {
+            document.location.href = "./attemptTest.html";
+            clearInterval(intr);
+        }
+        e.innerHTML = `<span>${time}</span>`;
+        if (time == 1) {
+            e.innerHTML = `<span>Let's Go!</span>`;
+        }
+        time--;
+    }, 1000);
+}
+function startCountDown(testTypeId, Name) {
+    $("header").hide();
+    sessionStorage.setItem("TestType", testTypeId);
+    sessionStorage.setItem("QuizName", Name);
+    sessionStorage.setItem("TestStart", true);
+    $("#quesContainer").show();
+
+    $(".testCard").hide();
+    document.body.style.backgroundColor = "#0d1229";
+    countNum();
+}
+let e = document.querySelector(".counter");
+e.innerHTML = `<span></span>`;
+function stopCountDown() {
+    var ques = confirm("Do you want to continue?");
+    if (ques) {
+        $("#quesContainer").hide();
+        $(".testCard").show();
+        document.body.style.backgroundColor = "white";
+        let e = document.querySelector(".counter");
+        e.innerHTML = `<span></span>`;
+
+        clearInterval(intr);
+    }
+}
---
+++ js/resultTest.js
@@ -0,0 +1,109 @@
+$(document).ready(() => {
+    if (!JSON.parse(sessionStorage.getItem("showResult"))) {
+        $("body").css("filter", "blur(8px)");
+        setTimeout(() => {
+            alert("Invalid Entry");
+            location.href = "./availableTest.html";
+        }, 1000);
+    } else {
+        sessionStorage.setItem("showResult", false);
+    }
+});
+$("#MarksObtained").text(
+    isNaN(sessionStorage.getItem("YourScore"))
+        ? 0
+        : sessionStorage.getItem("YourScore")
+);
+$("#MarksMax").text(sessionStorage.getItem("totalQues"));
+
+//answer attempted
+function quesSkeleton(questionNo, question, options, correctIndex) {
+    // opt=option
+    var points = 0;
+    var answerAttempted = sessionStorage
+        .getItem("answerAttempted")
+        .split(",");
+    console.log(correctIndex, answerAttempted[questionNo]);
+    $("#result").attr("class", "container");
+    var tDiv = $("<div></div>").attr("id", "timerDiv");
+    var tP = $("<h2></h2>").attr("id", "timer").addClass(" text-center");
+    tDiv.append(tP);
+    var bDiv = $("<div></div")
+        .addClass("border")
+        .attr("id", "border" + questionNo);
+    var bgColor = "bg-info";
+    if (answerAttempted[questionNo] == correctIndex) {
+        bgColor = "bg-success";
+        points = 1;
+    } else if (answerAttempted[questionNo] < 4) {
+        bgColor = "bg-danger";
+        points = 0;
+    } else {
+        bgColor = "bg-secondary";
+        points = 0;
+    }
+    var quizDiv = $("<div></div>").attr(
+        "class",
+        "  p-3 border-bottom " + bgColor
+    );
+    var quizNameDiv = $("<div></div>").attr(
+        "class",
+        "d-flex flex-row justify-content-between align-items-center"
+    );
+    var quizNameH = $("<h4></h4>").text("Marks: " + points);
+    if (points == 0) {
+        quizNameH.text(" Answer: " + options[correctIndex]);
+    }
+    var quizNumberSpan = $("<span></span>").text(
+        questionNo + 1 + " of " + totalQues
+    );
+    quizNameDiv.append(quizNameH, quizNumberSpan);
+    quizDiv.append(quizNameDiv);
+    var qaDiv = $("<div></div>")
+        .attr("id", "quesAnsDiv" + questionNo)
+        .addClass("bg-white p-3 border-bottom ");
+    var qDiv = $("<div></div>")
+        .attr("id", "quesDiv")
+        .addClass("d-flex flex-row align-items-center question-title");
+    var quesSymbol = $("<h3></h3>").addClass("text-danger").text("Q.");
+    // console.log(question);
+    var quesH = $("<H5></H5>")
+        .attr("id", "ques" + questionNo)
+        .addClass("mt-1 ml-2")
+        .text(question);
+    qDiv.append(quesSymbol, quesH);
+    var aDiv = $("<div></div>").attr("id", "ansDiv").addClass("ans ml-2");
+    // console.log(answerAttempted);
+
+    for (let k = 0; k < options.length; k++) {
+        var lb = $("<label></label>").addClass("radio");
+        var aI = $("<input/>")
+            .attr("name", questionNo)
+            .attr("type", "radio")
+            .attr("value", k);
+        // console.log(k);
+        aI.attr("disabled", "disable");
+        if (k == answerAttempted[questionNo]) aI.attr("checked", true);
+        var optionSpan = $("<span></span>").text(options[k]);
+        lb.append(aI, optionSpan);
+        aDiv.append(lb);
+    }
+    qaDiv.append(qDiv, aDiv);
+    bDiv.append(quizDiv, qaDiv);
+    $("#result").append(tDiv, bDiv);
+
+}
+let mcq = JSON.parse(sessionStorage.getItem("mcq"));
+var totalQues = mcq.questions.length;
+mcq = mcq.questions;
+console.log(mcq);
+
+for (let i = 0; i < totalQues; i++) {
+    quesSkeleton(i, mcq[i].question, mcq[i].answers, mcq[i].correctIndex);
+}
+function onHomeClick() {
+    let resultArr = [];
+    sessionStorage.setItem("resultShow", false);
+    sessionStorage.clear();
+    location.href = "./availableTest.html";
+}
